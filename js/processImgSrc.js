c23cab936dd949ef0e3dc38927c2a2ac1a6f9d21
grants <grants@qq.com>
2021-09-21 11:45:22 +0800

    add

--- js/processImgSrc.js
+++ js/processImgSrc.js
@@ -2,7 +2,7 @@ $(function() {
 	// process image path problem
 	$("img").each(function() {
 		var oldsrc = $(this).attr("src");
-		var newsrc = oldsrc.replace("..\project\mywiki\i7haowiki","");
+		var newsrc = oldsrc.replace("../project/mywiki/i7haowiki","");
 		$(this).attr("src", newsrc);
 	});
 })
